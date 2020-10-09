177ce6f9287886e1d4e7e26140bdf0d754695592
FridaPolished <fridapulido@gmail.com>
2020-10-08 18:14:37 -0700

    Add notes and mini exercises on data types in Java

--- new.java
+++ new.java
@@ -1,6 +1,54 @@
-public class Main {
+import java.util.Date;
+import java.awt.*;
 
+public class Main {
   public static void main(String[] args){
-    System.out.println("hi");
+    String message = "Hello" + "!";
+    boolean flag = message.endsWith("!");
+    int length = message.length();
+    String newMessage1 = message.repeat(2);
+    String newMessage2 = message.replace("o", "*");
+    String newMessage3 = message.toLowerCase();
+    String message2 = "  Hello! I am learning Java.";
+    String newMessage4 = message2.toLowerCase();
+    String newMessage5 = message2.trim();
+    
+
+    System.out.println(message);
+    System.out.println(flag);
+    System.out.println(length);
+    System.out.println(newMessage1);
+    System.out.println(newMessage2);
+    System.out.println(newMessage3);
+    System.out.println(newMessage4);
+    System.out.println(newMessage5);
   }
+  // public static void main(String[] args){
+  //   Point point1 = new Point(1, 1);
+  //   Point point2 = point1;
+  //   point1.x = 2;
+  //   System.out.println(point1);
+  //   System.out.println(point2);
+  // }
+  // public static void main(String[] args){
+  //   byte x = 1;
+  //   byte y = x;
+    
+  //   x = 2;
+  //   System.out.println(x);
+  // }
+  // public static void main(String[] args){
+  //   byte age = 30;
+  //   long views = 3_123_456_789L;
+  //   float price = 10.99F;
+  //   char letter = 'h';
+  //   boolean boo = true;
+  //   Date now = new Date();
+
+  //   now.getTime();
+
+  //   System.out.println(age);
+  //   System.out.println(price);
+  //   System.out.println(now);
+  // }
 }
---
+++ notes.md
@@ -0,0 +1,25 @@
+Data Types
+
+- Primitive
+- Reference
+
+Primitive data types
+Type    Bites       Range
+bite      1       [-128, 127]
+short     2        [-32, 32k]
+int       4        [-2, 2B]
+long      8
+float     4
+double    8
+char      2
+boolean   1
+
+
+with integers we can save values up to 2 million and we can use underscores to separate values.
+  123_456_789
+
+When numbers are larger than that we use sufix L;
+
+  3_123_456_789L
+
+  
